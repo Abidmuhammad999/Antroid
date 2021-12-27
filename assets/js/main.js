3b64cc38dc42eecad8e94a8a718129d2d207d852
Abidmuhammad999 <adm999adm999@gmail.com>
2021-12-27 21:15:00 +0530

    new commit

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+const words = ["Open Ecosystem", "Customizable UI", "Open Source", "Affordable"],
+  colors = ["blue", "green", "yellow", "red"],
+  text = document.querySelector(".text");
+
+// Generator (iterate from 0-3)
+function* generator() {
+  var index = 0;
+  while (true) {
+    yield index++;
+
+    if (index > 3) {
+      index = 0;
+    }
+  }
+}
+
+// Printing effect
+function printChar(word) {
+  let i = 0;
+  text.innerHTML = "";
+  text.classList.add(colors[words.indexOf(word)]);
+  let id = setInterval(() => {
+    if (i >= word.length) {
+      deleteChar();
+      clearInterval(id);
+    } else {
+      text.innerHTML += word[i];
+      i++;
+    }
+  }, 300);
+}
+
+// Deleting effect
+function deleteChar() {
+  let word = text.innerHTML;
+  let i = word.length - 1;
+  let id = setInterval(() => {
+    if (i >= 0) {
+      text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
+      i--;
+    } else {
+      text.classList.remove(colors[words.indexOf(word)]);
+      printChar(words[gen.next().value]);
+      clearInterval(id);
+    }
+  }, 10);
+}
+
+// Initializing generator
+let gen = generator();
+
+printChar(words[gen.next().value]);
