29da9123513b3278f7a4138afc09b7ee81f25bc7
Abidmuhammad999 <adm999adm999@gmail.com>
2022-01-04 13:14:12 +0530

    add footer some

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+
+//active nav bar
+// var element = document.getElementsByClassName("navbar-nav")[0];
+// element.addEventListener("click", myFunction);
+// function myFunction(e) {
+// 		var elems = document.querySelector(".active");
+//     if(elems !=null) {
+//       elems.classList.remove("active");
+//     }
+//     e.target.className = "active";
+// 	}
+
+
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
